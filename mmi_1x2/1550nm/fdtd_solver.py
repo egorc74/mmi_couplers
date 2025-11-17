b767781d033bb4c47e5807044af4112966272952
egorc74 <gorkaprod12@gmail.com>
2025-11-17 15:50:48 +0100

    added spedio scripts for 1x2 mmi

---
+++ mmi_1x2/1550nm/fdtd_solver.py
@@ -0,0 +1,170 @@
+from variables import *
+from geometry import geometry
+import plotly.graph_objects as go
+def fdtd_solver(sim,filename,width_ridge,mmi_length,taper_width,taper_width_in,mesh_accuracy,delta_y):
+
+    log = setup_logger("fdtd_solver", "logging/fdtd_solver.log")
+
+    log.info(f"Starting fdtd_solver() \n width_ridge: {width_ridge}, \n mmi_length:{mmi_length},\n taper_width={taper_width},\n ")
+    geometry(sim=sim,filename=filename,width_ridge=width_ridge,
+            mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,delta_y=delta_y)
+  
+
+    ##### FDTD dimensions #######
+    margin=10e-6
+    height_margin=0.5e-6
+    Xmin=-(mmi_length+2*wg_length+margin)/2 
+    Xmax=(mmi_length+2*wg_length+margin)/2
+    Zmin=-0.8e-6
+    Zmax=thick_Si3N4 + height_margin
+    width_margin=0.5e-6
+
+    Y_span=2*width_margin + width_ridge 
+    Ymin=-Y_span/2 
+    Ymax=-Ymin
+    
+    x_span=wg_length
+    width_eff=width_ridge
+    distance_wg=width_eff/4  ##effective distance
+
+
+
+
+    
+    ##Add fdtd
+    sim.addfdtd()
+    fdtd_margin=2e-6
+    sim.set("x min",Xmin)
+    sim.set("x max",Xmax)
+    sim.set("y",0)
+    sim.set("y span",Y_span+2e-6)
+    sim.set("z min",-0.5e-6)
+    sim.set("z max",0.5e-6)
+    sim.set("mesh accuracy", mesh_accuracy)
+    sim.set("x min bc","PML")  
+    sim.set("x max bc","PML")
+    sim.set("y min bc","PML") 
+    sim.set("y max bc","PML")
+    sim.set("z min bc","PML") 
+    sim.set("z max bc","PML")
+
+
+
+
+    #add source
+    sim.setglobalsource("wavelength start",wavelength)
+    sim.setglobalsource("wavelength stop",wavelength)
+
+    sim.setglobalmonitor("use source limits",0)
+    sim.setglobalmonitor("frequency points",1)
+    sim.setglobalmonitor("wavelength center",wavelength)
+
+
+    #Input port
+    sim.addport()
+    sim.set("name","source_port")
+    sim.set("injection axis","x-axis")
+    sim.set("direction","forward")
+    sim.set("y",0) 
+    wg_spacing=width_ridge/3
+    sim.set("y span",wg_spacing)
+    sim.set("x",Xmin+2e-6) 
+    sim.set("z min",Zmin) 
+    sim.set("z max",Zmax)
+
+
+
+    ##Through port
+
+    sim.addport()
+    sim.set("name","through_port")
+    sim.set("injection axis","x-axis")
+    sim.set("direction","backward")
+            
+    sim.set("y span",wg_spacing)
+    sim.set("y",-distance_wg+delta_y) 
+    sim.set("y span",wg_spacing)
+    sim.set("x",Xmax-2e-6) 
+    sim.set("z min",Zmin) 
+    sim.set("z max",Zmax)
+
+    ##Cross port 
+    sim.addport()
+    sim.set("name","cross_port")
+    sim.set("injection axis","x-axis")
+    sim.set("direction","backward")
+    sim.set("y span",wg_spacing)
+    sim.set("y",distance_wg-delta_y) 
+    sim.set("x",Xmax-2e-6)
+    sim.set("z min",Zmin) 
+    sim.set("z max",Zmax) 
+
+    sim.adddftmonitor()
+    sim.set("x span",200e-6)
+    sim.set("y span",20e-6)
+    sim.set("x",0)
+    sim.set("y",0)
+    sim.set("z",0)
+
+
+    sim.save(f"{filename}.fsp")
+    
+    #run fdtd
+
+    # sim.run()
+
+    # #get results from both monitors
+    m1_name="cross_port"
+    m2_name="through_port"
+    try:
+        T_cross = sim.getresult(m1_name,"T")
+        T_bar = sim.getresult(m2_name,"T")
+        log.info(f"Obtained T_cross {T_cross} and T_bar={T_bar}")
+
+    except Exception as e:
+        T_cross=0
+        T_bar=0
+        log.error(f"Error occured: {e} Obtained T_cross {T_cross} and T_bar={T_bar}")
+
+    input("Press Enter to continue...")
+
+    sim.save(f"{filename}.fsp")
+
+    return T_cross,T_bar
+
+
+
+
+
+
+if __name__=="__main__":
+    import os
+
+    filename="mmi_1x2_fdtd"
+    wg_length=15e-6
+    wg_width=1.6e-6
+    width_ridge=11e-6
+    mmi_length=61e-6
+    taper_width=4.4e-6
+    taper_width_in=4.4e-6
+    delta_y=0e-6
+    n_core=1.9963
+    cladding=0
+
+    #define ratio
+    mesh_accuracy=3
+    if os.path.isfile(f"{filename}.fsp"):
+        fdtd_solver(sim=lumapi.FDTD(filename),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy)
+
+    else:
+        fdtd_solver(sim=lumapi.FDTD(),filename=filename,wg_width=wg_width,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy)
+
+
+
+
+
+
+
+
---
+++ mmi_1x2/1550nm/speedio.py
@@ -0,0 +1,139 @@
+from variables import *
+from fdtd_solver import *
+
+log = setup_logger("speedio", "logging/speedio.log")
+
+
+def Length_sweep(sim):
+
+    filename="speedio_test"
+    wg_length=40e-6
+    wg_width=1.6e-6
+    width_ridge=11e-6
+    mmi_length=61e-6
+    taper_width=4.4e-6
+    taper_width_in=taper_width
+
+    delta_y=0e-6
+    n_core=1.9963
+    cladding=0
+
+    mesh_accuracy=3
+    
+    Lengths=np.linspace(mmi_length-10e-6,mmi_length+10e-6,21)
+    T_cross_values=[]
+    T_bar_values=[]
+    log.info(f"Starting Length Sweep \n Length_span: {Lengths}, \n width_ridge: {width_ridge}, \n mmi_length:{mmi_length},\n taper_width={taper_width}")
+
+    for mmi_length in Lengths:
+        if os.path.isfile(f"{filename}.fsp"):
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(filename),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+
+            
+        else:
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+        T_cross_values.append(T_cross)
+        T_bar_values.append(T_bar)
+    np.savez('data/Length_sweep.npz', Lengths=Lengths,T_cross_values=T_cross_values, T_bar_values=T_bar_values)
+
+
+
+def Width_sweep(sim):
+    filename="speedio_test"
+    wg_length=40e-6
+    wg_width=1.6e-6
+    width_ridge=11e-6
+    mmi_length=61e-6
+    taper_width=4.4e-6
+    taper_width_in=taper_width
+    delta_y=0e-6
+    n_core=1.9963
+    cladding=0
+
+
+    mesh_accuracy=3
+    
+    Widths=np.linspace(width_ridge-0.5e-6,width_ridge+0.5e-6,11)
+    T_cross_values=[]
+    T_bar_values=[]
+    log.info(f"Starting Width sweep\n Width_span: {Widths}, \n width_ridge: {width_ridge}, \n mmi_length:{mmi_length},\n taper_width={taper_width}")
+
+
+    for width_ridge in Widths:
+        ###move tapers so that the gap is kept as 1.1um
+        delta_y=(width_ridge/2-1.1e-6-taper_width)/2
+        if os.path.isfile(f"{filename}.fsp"):
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(filename),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+            
+        else:
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+        T_cross_values.append(T_cross)
+        T_bar_values.append(T_bar)
+    np.savez('data/Width_sweep.npz', Widths=Widths,T_cross_values=T_cross_values, T_bar_values=T_bar_values)
+
+
+
+def Taper_width_sweep(sim):
+    filename="speedio_test"
+    wg_length=40e-6
+    wg_width=1.6e-6
+    width_ridge=11e-6
+    mmi_length=61e-6
+    taper_width=4.4e-6
+    taper_width_in=taper_width
+    delta_y=0e-6
+    n_core=1.9963
+    cladding=0
+
+
+    mesh_accuracy=3
+    
+    Taper_widths=np.linspace(taper_width-0.5e-6,taper_width+0.5e-6,11)
+    T_cross_values=[]
+    T_bar_values=[]
+    log.info(f"Starting Width sweep\n Width_span: {Taper_widths}, \n width_ridge: {width_ridge}, \n mmi_length:{mmi_length},\n taper_width={taper_width}")
+
+
+    for taper in Taper_widths:
+        ###move tapers so that the gap is kept as 1.1um
+        taper_width=taper
+        taper_width_in=taper
+        if os.path.isfile(f"{filename}.fsp"):
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(filename),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+            
+        else:
+            T_cross,T_bar=fdtd_solver(sim=lumapi.FDTD(),filename=filename,width_ridge=width_ridge,
+             mmi_length=mmi_length,taper_width=taper_width,taper_width_in=taper_width_in,mesh_accuracy=mesh_accuracy,delta_y=delta_y)
+
+        T_cross_values.append(T_cross)
+        T_bar_values.append(T_bar)
+    np.savez('data/Taper_width_sweep.npz', Taper_widths=Taper_widths,T_cross_values=T_cross_values, T_bar_values=T_bar_values)
+
+
+
+
+
+if __name__ =="__main__":
+    filename="speedio_test"
+    if os.path.isfile(f"{filename}.fsp"):
+        # Length_sweep(sim=lumapi.FDTD(filename))
+        # Width_sweep(sim=lumapi.FDTD(filename))
+        # Taper_width_sweep(sim=lumapi.FDTD(filename))
+
+        pass
+    else:
+        # Length_sweep(sim=lumapi.FDTD())
+        # Width_sweep(sim=lumapi.FDTD())
+        # Taper_width_sweep(sim=lumapi.FDTD())
+
+        pass
